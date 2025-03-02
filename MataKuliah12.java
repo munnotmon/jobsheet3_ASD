78063b2ea3d71369b0551f0b5ae7f9c203dce292
maulidaprina <mldaprna@gmail.com>
2025-03-02 21:14:52 +0700

    Percobaan 3.4

---
+++ MataKuliah12.java
@@ -0,0 +1,13 @@
+public class MataKuliah12 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public MataKuliah12(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
